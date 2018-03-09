669387cee862a073b2a6f5228164f5b53cf278d2
Jeremy Noh <jeremynoh@MacBook-Pro-de-Wilson.local>
2018-03-09 12:20:54 +0100

    add change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({Hi : "Hello"});
+  res.json({Hi : "Hello :) "});
 });
 
 module.exports = router;
