ad70ff0c23e54a827b387bd0d151f2da056303db
Jeremy Noh <jeremynoh@MacBook-Pro-de-Wilson.local>
2018-03-09 16:10:08 +0100

    ajout user , models

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var app = express();
+const db = require('./db/init');
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+
 var express = require('express');
 var router = express.Router();
+const db = require('../db/init')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({Hi : "Hello :) "});
+	db.user.sync().then(() => {
+		 db.user.create({
+  	firstname: 'Jeremy',
+  	lastname: 'nohile',
+  	email: 'jeremy.nohile@gmail.com'
+  	}).then(user => {
+  		res.json({user: user})
+  })
+})
+
 });
 
 module.exports = router;
